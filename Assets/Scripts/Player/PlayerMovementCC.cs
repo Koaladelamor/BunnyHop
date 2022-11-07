1f621460f2a50f99de7a781d5314bda16cddabd5
marKinhu <95031286+Koaladelamor@users.noreply.github.com>
2022-11-07 23:29:43 +0100

    mas es menos

--- Assets/Scripts/Player/PlayerMovementCC.cs
+++ Assets/Scripts/Player/PlayerMovementCC.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using static UnityEditor.PlayerSettings;
 using Debug = UnityEngine.Debug;
 
@@ -15,13 +16,34 @@ public class PlayerMovementCC : MonoBehaviour
 
     private Vector3 finalVelocity = Vector3.zero;
 
+    [System.Serializable]
+    public class MovementSettings
+    {
+        public float MaxSpeed;
+        public float CurrentSpeed;
+        public float Acceleration;
+        public float Deceleration;
+
+        public MovementSettings(float maxSpeed, float accel, float decel)
+        {
+            MaxSpeed = maxSpeed;
+            CurrentSpeed = 0;
+            Acceleration = accel;
+            Deceleration = decel;
+        }
+    }
+
     public Camera playerCam;
     private CharacterController player;
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float currentSpeed;
-    [SerializeField] private float acceleration;
+
+    [Header("Movement")]
+
     [SerializeField] private float gravity;
+    [SerializeField] private float friction;
     [SerializeField] private float jumpForce;
+    [SerializeField] private MovementSettings m_GroundSettings = new MovementSettings(7, 14, 10);
+    [SerializeField] private MovementSettings m_AirSettings = new MovementSettings(7, 2, 2);
+    [SerializeField] private MovementSettings m_StrafeSettings = new MovementSettings(1, 50, 50);
     private bool validJump;
 
     [SerializeField] private float offsetY;
@@ -40,13 +62,13 @@ public class PlayerMovementCC : MonoBehaviour
         inputMovement.Normalize();
 
         //ALEX MOVEMENT
-        Vector3 directionCameraRelative = inputMovement.z * playerCam.transform.forward + inputMovement.x * playerCam.transform.right;
-        Vector3 orientation = Quaternion.Euler(0f, playerCam.transform.eulerAngles.y, 0f) * new Vector3(directionCameraRelative.x, 0f, directionCameraRelative.z);
-        Debug.Log("Direction: " + directionCameraRelative.x + "  " + directionCameraRelative.z);
-
+        Vector3 desiredMovement = inputMovement.z * playerCam.transform.forward + inputMovement.x * playerCam.transform.right;
+        Vector3 orientation = Quaternion.Euler(0f, playerCam.transform.eulerAngles.y, 0f) * new Vector3(desiredMovement.x, 0f, desiredMovement.z);
+        //Debug.Log("Direction: " + directionCameraRelative.x + "  " + directionCameraRelative.z);
+        //Debug.Log("Input: " + inputMovement.x + "  " + inputMovement.y + "  " + inputMovement.z);
 
         // Y direction
-        directionCameraRelative.y = -1f;
+        desiredMovement.y = -1f;
 
         if (player.isGrounded)
         {
@@ -60,69 +82,95 @@ public class PlayerMovementCC : MonoBehaviour
             else
             {
                 // On Ground
-                finalVelocity.y = directionCameraRelative.y * gravity * Time.deltaTime;
+                finalVelocity.y = desiredMovement.y * gravity * Time.deltaTime;
 
-                //Friction
-                if (directionCameraRelative.x == 0.0f && directionCameraRelative.z == 0.0f)
+                //Friction: Needs to skip 1 frame on landing
+                if (desiredMovement.x == 0.0f && desiredMovement.z == 0.0f)
                 {
-                    if (currentSpeed > 0)
+                    if (m_GroundSettings.CurrentSpeed >= 0)
                     {
-                        currentSpeed -= Time.deltaTime * 8;
+                        m_GroundSettings.CurrentSpeed -= Time.deltaTime * 2;
                     }
                 }
                 // Calculate ground speed
-                else if (directionCameraRelative.z != 0.0f)
+                else if (desiredMovement.z != 0.0f)
                 {
-                    if (currentSpeed <= maxSpeed)
+                    if (m_GroundSettings.CurrentSpeed <= m_GroundSettings.MaxSpeed)
                     {
-                        currentSpeed += acceleration * Time.deltaTime;
+                        m_GroundSettings.CurrentSpeed += m_GroundSettings.Acceleration * Time.deltaTime;
                     }
-                    else if (currentSpeed > maxSpeed)
+                    else if (m_GroundSettings.CurrentSpeed > m_GroundSettings.MaxSpeed)
                     {
-                        currentSpeed = maxSpeed;
+                        m_GroundSettings.CurrentSpeed = m_GroundSettings.MaxSpeed;
                     }
                 }
+
+                // XZ movement
+                finalVelocity.x = desiredMovement.x * m_GroundSettings.CurrentSpeed;
+                finalVelocity.z = desiredMovement.z * m_GroundSettings.CurrentSpeed;
 
             }
         }
         else
         {
-            // Strafe Jump
-
+            // Air Velocity
 
             // Gravity
-            finalVelocity.y += directionCameraRelative.y * gravity * Time.deltaTime;
+            finalVelocity.y += desiredMovement.y * gravity * Time.deltaTime;
 
             // Raycasts to calculate ground and validate jump
             Vector3 playerFeet = transform.position + new Vector3(0, -1f, 0);
 
-            //Debug.DrawLine(playerFeet + new Vector3(0.2f, 0, 0), playerFeet + new Vector3(0.2f, offsetY, 0));
-            //Debug.DrawLine(playerFeet + new Vector3(-0.2f, 0, 0), playerFeet + new Vector3(-0.2f, offsetY, 0));
-
             if (Physics.Linecast(playerFeet + new Vector3(0.2f, 0, 0), playerFeet + new Vector3(0.2f, offsetY, 0)) && Manager_Input._INPUT_MANAGER.GetJumpButtonDown())
             {
                 validJump = true;
-            };
+            }
 
             if (Physics.Linecast(playerFeet + new Vector3(-0.2f, 0, 0), playerFeet + new Vector3(-0.2f, offsetY, 0)) && Manager_Input._INPUT_MANAGER.GetJumpButtonDown())
             {
                 validJump = true;
-            };
-
+            }
+            //Debug.DrawLine(playerFeet + new Vector3(0.2f, 0, 0), playerFeet + new Vector3(0.2f, offsetY, 0));
+            //Debug.DrawLine(playerFeet + new Vector3(-0.2f, 0, 0), playerFeet + new Vector3(-0.2f, offsetY, 0));
         }
-
-        // XZ movement
-        finalVelocity.x = directionCameraRelative.x * currentSpeed;
-        finalVelocity.z = directionCameraRelative.z * currentSpeed;
-
-
 
         // Physically move player
         player.Move(finalVelocity * Time.deltaTime);
         transform.rotation = Quaternion.LookRotation(orientation * Time.deltaTime);
 
         //Debug.Log("vel z: " + finalVelocity.z + " vel x: " + finalVelocity.x);
+        Debug.Log("vel z: " + finalVelocity.z + " vel x: " + finalVelocity.x + " vel y: " + finalVelocity.y);
         //Debug.Log("Velocity XZ: " + Vector3.SqrMagnitude(new Vector3(player.velocity.x, 0, player.velocity.z)));
+    }
+
+    private void ApplyFriction(float t)
+    {
+        Vector3 vec = finalVelocity; // Equivalent to: VectorCopy();
+        float speed;
+        float newspeed;
+        float control;
+        float drop;
+
+        vec.y = 0.0f;
+        speed = vec.magnitude;
+        drop = 0.0f;
+
+        /* Only if the player is on the ground then apply friction */
+        if (player.isGrounded)
+        {
+            control = speed < m_GroundSettings.Deceleration ? m_GroundSettings.Deceleration : speed;
+            drop = control * friction * Time.deltaTime * t;
+        }
+
+        newspeed = speed - drop;
+        friction = newspeed;
+        if (newspeed < 0)
+            newspeed = 0;
+        if (speed > 0)
+            newspeed /= speed;
+
+        finalVelocity.x *= newspeed;
+        finalVelocity.z *= newspeed;
     }
 
 
